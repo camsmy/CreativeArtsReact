fe8cce440a0f685d53bf3e3b4984fb650c57b1f8
Camille <camille.monis@gmail.com>
2023-02-20 07:55:58 +0800

    cleanhomepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -56,29 +56,6 @@ const Home = () => {
         </Container>
       </div>
 
-      <select class="form-select" id="multiple-select-field" data-placeholder="Choose anything" multiple>
-    <option>Christmas Island</option>
-    <option>South Sudan</option>
-    <option>Jamaica</option>
-    <option>Kenya</option>
-    <option>French Guiana</option>
-    <option>Mayotta</option>
-    <option>Liechtenstein</option>
-    <option>Denmark</option>
-    <option>Eritrea</option>
-    <option>Gibraltar</option>
-    <option>Saint Helena, Ascension and Tristan da Cunha</option>
-    <option>Haiti</option>
-    <option>Namibia</option>
-    <option>South Georgia and the South Sandwich Islands</option>
-    <option>Vietnam</option>
-    <option>Yemen</option>
-    <option>Philippines</option>
-    <option>Benin</option>
-    <option>Czech Republic</option>
-    <option>Russia</option>
-</select>
-
     </>
   )
 }
